1a4a6740fc536c4e51592a4999a3501ca93c64bf
Zaid Alhir <zaidalhir@yahoo.com>
2019-10-28 21:00:20 -0500

    giphy with key, adding new animal button

--- java.js
+++ java.js
@@ -1,10 +1,9 @@
-// array of animals 
 $(document).ready(function(){
     const animals = ["cat", "dog", "rabbit", "cow", "tiger"];
 
     function createButtons(){
         $(".animal-buttons").empty();
-        for(var i=0; i < animals.length; i++){
+        for(let i=0; i < animals.length; i++) {
             const button = $("<button>");
             button.addClass("animalButton");
             button.attr("data-type", animals[i]);
@@ -16,8 +15,33 @@ $(document).ready(function(){
     $(document).on("click", ".animalButton", function(){
         const name = $(this).attr("data-type");
         console.log("name: " +name);
+        
+        // url with key
+    
 
+        const queryUrl = `https://api.giphy.com/v1/gifs/search?q=${name}&api_key=lce25bMPzWCIbAFmwtBgFtub26pRDd58&limit=10`;
+        $.ajax({
+            url: queryUrl,
+            method: "GET"
+        })
+        .then(function(response){
+            console.log(queryUrl);
 
+            console.log(response);
+
+           
+            const animlas = response.data;
+
+            console.log(data);
+        })
     })
-
+    $("#add-animal").on("click", function(event){
+        event.preventDefault();
+        const newAnimal = $("input").val();
+        console.log("new Animal: " +newAnimal);
+        animals.push(newAnimal);
+        createButtons();
+    })
 })
+
+
